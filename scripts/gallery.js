3b331fd79262418e9a89ddf7ce544bc23805f359
nefierez <nefierez@byui.edu>
2023-03-27 19:38:48 -0600

    Photo session type being displayed if type is selected on the gallery page. Still not working on the index page

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,10 +1,10 @@
 import PhotoSession from "./gallery.mjs";
 import sessionSelector from "./sessionSelector.mjs";
 
-const session = new PhotoSession();
-session.photoSearch("wedding ceremony");
 
-const session_Selector = sessionSelector();
 
-const type = session_Selector.getSelectedSession();
-console.log(type);
+sessionSelector()
+    .then((selectedValue) => {
+        const session = new PhotoSession();
+        session.photoSearch(selectedValue);
+    })
--- scripts/sessionSelector.mjs
+++ scripts/sessionSelector.mjs
@@ -1,16 +1,11 @@
 export default function sessionSelector() {
-    let querySearch = '';
-
-    const sessionTypes = document.querySelectorAll(".sessionType");
-    sessionTypes.forEach((sessionType) => {
-        sessionType.addEventListener("click", () => {
-            querySearch = Array.from(sessionType.classList).slice(1).join(' ');
+    return new Promise((resolve) => {
+        const sessionTypes = document.querySelectorAll(".sessionType");
+        sessionTypes.forEach((sessionType) => {
+            sessionType.addEventListener("click", () => {
+                const querySearch = Array.from(sessionType.classList).slice(1).join(' ');
+                resolve(querySearch);
+            });
         });
     });
-
-    return {
-        getSelectedSession() {
-            return querySearch;
-        }
-    };
 }
